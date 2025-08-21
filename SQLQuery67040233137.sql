ba107273cf50c726915f86a39000c2cc88a00a21
Thanaphon <flck7115@gmail.com>
2025-08-21 14:16:45 +0700

    Update SQLQuery67040233137.sql

--- SQLQuery67040233137.sql
+++ SQLQuery67040233137.sql
@@ -26,3 +26,20 @@ FROM Orders JOIN Shippers
 ON Shippers.ShipperID=Orders.Shipvia
 WHERE OrderID=10275
 
+Select p. ProductID, p.ProductName, s. CompanyName, s. Country
+from products p Join Suppliers s on p.SupplierID = s. SupplierID
+where Country in ('usa', 'uk')
+
+select e.EmployeeID, FirstName, o.OrderID
+from Employees e join orders o on e.EmployeeID = o. EmployeeID
+order by EmployeeID
+
+SELECT O.OrderID เลขใบสั่งซื้อ, C.CompanyName ลูกค้า,E.FirstName พนักงาน, O.ShipAddress ส่งไปที่
+FROM Orders O, Customers C, Employees E
+WHERE O.CustomerID=C.CustomerID
+AND O.EmployeeID=E.EmployeeID
+
+SELECT O.OrderID เลขใบสั่งซื้อ, C.CompanyName ลูกค้า, E.FirstName พนักงาน, O.ShipAddress ส่งไปที่
+FROM Orders O
+join Customers C on O.CustomerID=C.CustomerID
+join Employees E on O.EmployeeID=E.EmployeeID
