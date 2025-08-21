6cc343bbc718f2d143429ac48743e340730435dc
Thanaphon <flck7115@gmail.com>
2025-08-21 13:53:32 +0700

    Update SQLQuery67040233137.sql

--- SQLQuery67040233137.sql
+++ SQLQuery67040233137.sql
@@ -1,3 +1,28 @@
-Select CategoryName, ProductName, UnitPrice
-From Products, Categories
-Where products.CategoryID=Categories.CategoryID
+  Select CategoryName, ProductName, UnitPrice
+  From Products, Categories
+  Where products.CategoryID = Categories.CategoryID
+  and CategoryName = 'seafood'
+
+  Select c.CategoryName, p.ProductName, P.UnitPrice
+  From product as p john Categories as c
+  on p.CategoryID = c.CategoryID
+  Where 
+
+  SELECT CompanyName, OrderID
+  FROM Orders, Shippers
+  WHERE Shippers.ShipperID = Orders.Shipvia
+
+  SELECT CompanyName, OrderID
+  FROM Orders JOIN Shippers
+  ON Shippers.ShipperID = Orders.Shipvia
+
+SELECT CompanyName, OrderID
+FROM Orders, Shippers
+WHERE Shippers.ShipperID = Orders.Shipvia
+AND OrderID = 10275
+
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID=Orders.Shipvia
+WHERE OrderID=10275
+
